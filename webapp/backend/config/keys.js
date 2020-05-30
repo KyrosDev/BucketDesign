3af2af473a0c398e1f6df300be9a09e9dd4f458a
KyrosDev <zKyros.maker@gmail.com>
2020-05-30 04:40:58 +0200

    Initial Commit

---
+++ webapp/backend/config/keys.js
@@ -0,0 +1,5 @@
+const dbPassword = "mongodb+srv://bdroot:9+ccaxhr@bucketdesigndb-c0ofs.mongodb.net/BD?retryWrites=true&w=majority";
+
+module.exports = {
+    mongoURI: dbPassword
+}
---
+++ webapp/backend/public/dir.js
@@ -0,0 +1,3 @@
+const dirname = __dirname + '/images/';
+
+module.exports = dirname
